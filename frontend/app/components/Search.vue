a5cea8400dbf37a840fbcbb4ff851f6fe5f471a2
Oliver1ck <artem131103@yandex.ru>
2026-03-02 09:35:24 +0300

    Implement search functionality with debounced input and dynamic product list

--- frontend/app/components/Search.vue
+++ frontend/app/components/Search.vue
@@ -1,17 +1,74 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import type { Product } from '../data/searchData'
+import { watchDebounced } from '@vueuse/core'
+import { searchList } from '../data/searchData'
+import Link from './Link.vue'
+
+const newSearchList = ref<Product[]>([])
+const searchModel = defineModel<string>('searchModel')
+const searchListVisible = ref<boolean>(false)
+const searchProductListClass = computed(() => [
+  'search--product--list',
+  { 'search--product--list--visible': searchListVisible.value },
+])
+watchDebounced(searchModel, (newValue: string | undefined) => {
+  if (!newValue) {
+    searchListVisible.value = false
+    newSearchList.value = []
+    return
+  }
+  for (const i of searchList) {
+    console.log(i)
+    if (i.name.toLowerCase().includes(newValue.toLowerCase())) {
+      newSearchList.value.push(i)
+      searchListVisible.value = true
+    }
+  }
+}, { debounce: 500 })
+function handleFocusOut() {
+  searchListVisible.value = false
+}
+function handleFocusIn() {
+  if (searchModel.value) {
+    searchListVisible.value = true
+  }
+}
+</script>
 
 <template>
   <form class="search">
-    <img src="@assets/img/icons/search.svg" alt="search icon" />
-    <input type="text" name="search" placeholder="Поиск" />
+    <div class="search--wrapper">
+      <img src="@assets/img/icons/search.svg" alt="search icon" />
+      <input
+        v-model="searchModel"
+        type="text"
+        name="search"
+        placeholder="Поиск"
+        @focusout="handleFocusOut"
+        @focusin="handleFocusIn"
+      />
+    </div>
+    <ul :class="searchProductListClass">
+      <li v-for="item in newSearchList" :key="item.name" class="item">
+        <Link to="/" variant="searchItem">
+          <template #icon>
+            <img :src="item.img" alt="product image" />
+          </template>
+          <template #default>
+            {{ item.name }}
+          </template>
+        </Link>
+      </li>
+    </ul>
   </form>
 </template>
 
 <style lang="scss" scoped>
 .search {
-  position: relative;
   flex: 0 1 31.25rem;
-
+  position: relative;
+}
+.search--wrapper {
   & input {
     border-radius: 0.25rem;
     border: 1px solid var(--Text-input-border);
@@ -34,6 +91,28 @@
     top: 50%;
     transform: translateY(-50%);
     left: 0.88rem;
+  }
+}
+
+.search--product--list {
+  transition: all 0.3s ease;
+  opacity: 0;
+  pointer-events: none;
+  width: 100%;
+  position: absolute;
+  max-height: 22.5rem;
+  top: 2.62rem;
+  padding: 0.5rem 0;
+  border-radius: 0.5rem;
+  background: var(--Surface-Default);
+  box-shadow:
+    0 0 2px 0 rgba(0, 0, 0, 0.2),
+    0 2px 10px 0 rgba(0, 0, 0, 0.1);
+  overflow: auto;
+
+  &--visible {
+    opacity: 1;
+    pointer-events: auto;
   }
 }
 </style>
---
+++ frontend/app/data/searchData.ts
@@ -0,0 +1,44 @@
+import productCatImage from '@assets/img/productCat.png'
+
+export interface Product {
+  name: string
+  img: string
+}
+export const searchList: Product[] = [
+  {
+    name: 'Лакомство РОДНЫЕ КОРМА для собак корень бычий цельный сушеный в дровяной печи',
+    img: productCatImage,
+  },
+  {
+    name: 'Лакомство РОДНЫЕ КОРМА для собак корень бычий цельный сушеный в дровяной печи',
+    img: productCatImage,
+  },
+  {
+    name: 'Лакомство РОДНЫЕ КОРМА для собак корень бычий цельный сушеный в дровяной печи',
+    img: productCatImage,
+  },
+  {
+    name: 'Сухой корм Royal Canin Sterilised 37 для стерилизованных кошек (2 кг)',
+    img: productCatImage,
+  },
+  {
+    name: 'Игрушка для собак мяч лайкер Liker Cord на шнуре, диаметр 7 см, оранжевый',
+    img: productCatImage,
+  },
+  {
+    name: 'Игрушка для котов мяч кряк Liker Cord на шнуре, диаметр 7 см, оранжевый',
+    img: productCatImage,
+  },
+  {
+    name: 'Шампунь для щенков гипоаллергенный "Нежный уход" с экстрактом ромашки, 250 мл',
+    img: productCatImage,
+  },
+  {
+    name: 'Когтеточка-столбик с лежанкой для крупных кошек, сизаль, 80 см, бежевая',
+    img: productCatImage,
+  },
+  {
+    name: 'Влажный корм Felix "Аппетитные кусочки" двойная вкуснятина (кролик и утка), 85 г',
+    img: productCatImage,
+  },
+]
