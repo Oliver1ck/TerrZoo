e53dc6579933db6a30935d6d2c8bf23dda5f5227
Oliver1ck <artem131103@yandex.ru>
2026-03-04 11:12:41 +0300

    Add Intro component and corresponding image; update index page to include Intro section

--- frontend/app/pages/index.vue
+++ frontend/app/pages/index.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import AnimalSection from '@components/Sections/AnimalSection.vue'
+import Intro from '@components/Sections/Intro.vue'
 </script>
 
 <template>
   <AnimalSection />
+  <Intro />
 </template>
 
 <style lang="scss" scoped>
