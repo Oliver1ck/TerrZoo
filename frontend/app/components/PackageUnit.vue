196aba5d44c212972e27b70d8fe3a776d05eb51f
Oliver1ck <artem131103@yandex.ru>
2026-03-11 13:06:09 +0300

    Add PackageUnit and PackageUnitList components for package display

---
+++ frontend/app/components/PackageUnit.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+import type { NumberOfPackages } from '../data/product'
+
+const {
+  unit = {
+    id: 1,
+    count: 85,
+    unit: 'г',
+  },
+  isActive = false,
+} = defineProps<{
+  unit?: NumberOfPackages
+  isActive?: boolean
+}>()
+
+const emit = defineEmits<{
+  (e: 'click', isActive: boolean): void
+}>()
+</script>
+
+<template>
+  <div
+    class="package-unit"
+    :class="{ 'package-unit--active': isActive }"
+    @click="emit('click', isActive)"
+  >
+    <Typography tag="p" variant="body-sm">
+      {{ unit.count }} {{ unit.unit }}
+    </Typography>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.package-unit {
+  display: flex;
+  align-items: center;
+  gap: 0.375rem;
+  border-radius: 0.125rem;
+  transition: all 0.3s ease;
+  padding: 0.12rem 0.5rem;
+  background: var(--Package-Unit-Default-Bg);
+  cursor: pointer;
+
+  &:hover {
+    background: var(--Border-Highlight-Default);
+    :deep(p) {
+      color: var(--Surface-Default);
+    }
+  }
+
+  &--active {
+    background: var(--Border-Highlight-Default);
+    :deep(p) {
+      color: var(--Surface-Default);
+    }
+  }
+}
+</style>
---
+++ frontend/app/components/PackageUnitList.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import type { NumberOfPackages } from '../data/product'
+
+import PackageUnit from './PackageUnit.vue'
+
+const props = defineProps<{
+  data: NumberOfPackages[]
+}>()
+
+const activeUnitId = ref<number | null>(null)
+
+function handleUnitClick(unitId: number, isActive: boolean) {
+  activeUnitId.value = unitId
+  if (isActive) {
+    activeUnitId.value = null
+  }
+}
+</script>
+
+<template>
+  <ul class="package-unit-list">
+    <li v-for="unit in data" :key="unit.id" class="package-unit-list-item">
+      <PackageUnit
+        :unit="unit"
+        :is-active="activeUnitId === unit.id"
+        @click="(isActive) => handleUnitClick(unit.id, isActive) "
+      />
+    </li>
+  </ul>
+</template>
+
+<style lang="scss" scoped>
+.package-unit-list {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 0.38rem 0.25rem;
+}
+</style>
