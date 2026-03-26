a5a66f7c1dd448cacba243839fef181e8fbd4d22
Oliver1ck <artem131103@yandex.ru>
2026-03-26 15:12:16 +0300

    rename articles folder

---
+++ app/pages/articles/[id].vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import BreadCrumbs from '@components/Sections/BreadCrumbs.vue'
+</script>
+
+<template>
+  <BreadCrumbs>
+    <template #product>
+      Какая-то статья
+    </template>
+  </BreadCrumbs>
+</template>
+
+<style lang="scss" scoped></style>
